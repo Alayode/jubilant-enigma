154820f47b036eff45b9c2edae067e17b4d5b1d4
Chris Samuel <ksamuel.chris@icloud.com>
2016-07-28 17:27:26 -0400

    added intro animation to begin game

--- js/quiz.js
+++ js/quiz.js
@@ -2,4 +2,26 @@
 
 	// script goes here
 
+	var app = angular.module('myQuiz',[]);
+
+	// strict contextual escaping
+	//for minification process and to prevent the minify script from changing the names
+	app.controller('QuizController',['$scope','$http','$sce', function($scope,$http,$sce){
+
+			$scope.score = 0 ;
+			$scope.activeQuestion = -1;
+			$scope.percentage = 0;
+
+			$http.get('quiz_data.json').then(function(quizData){
+					$scope.myQuestions = quizData.data;
+					$scope.totalQuestions = $scope.myQuestions.length;
+			});
+
+
+
+	}]);
+
+
+
+
 })();
