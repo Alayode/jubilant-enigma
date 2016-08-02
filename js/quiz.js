6de8b1990944e4b62c0f58601402b5f4687ab75e
Chris Samuel <ksamuel.chris@icloud.com>
2016-08-02 00:13:55 -0400

    add a perentage funtion to the scope object

--- js/quiz.js
+++ js/quiz.js
@@ -17,8 +17,36 @@
 					$scope.totalQuestions = $scope.myQuestions.length;
 			});
 
+			$scope.selectAnswer = function(qIndex,aIndex){
+				// console.log(qIndex +  ' and ' + aIndex );
 
+				var questionState = $scope.myQuestions[qIndex].questionState;
 
+				if( questionState != 'answered'){
+					$scope.myQuestions[qIndex].selectedAnswer = aIndex;
+					var correctAnswer = $scope.myQuestions[qIndex].correct;
+					$scope.myQuestions[qIndex].correctAnswer =correctAnswer;
+					if( aIndex === correctAnswer ){
+						$scope.myQuestions[qIndex].correctness = 'correct';
+						$scope.score += 1;
+					} else {
+						$scope.myQuestions[qIndex].correctness = 'incorrect';
+					}
+						$scope.myQuestions[qIndex].questionState = 'answered';
+				}
+				/* Percentage Meter */
+				$scope.percentage = ($scope.score / $scope.totalQuestions)  * 100 
+
+				$scope.isSelected = function(qIndex,aIndex){
+					return  $scope.myQuestions[qIndex].selectedAnswer === aIndex;
+				}
+				$scope.isCorrect = function(qIndex,aIndex){
+					return  $scope.myQuestions[qIndex].correctAnswer === aIndex;
+				}
+			}
+			$scope.selectContinue = function(){
+				return $scope.activeQuestion += 1;
+			}
 	}]);
 
 
